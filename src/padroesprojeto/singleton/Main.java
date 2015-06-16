cc13fbf7ee586b6bab2e283637b6b845e211b1b5
felansu <gaferran@gmail.com>
2015-06-16 00:06:39 -0300

    Singleton

---
+++ src/padroesprojeto/singleton/Main.java
@@ -0,0 +1,21 @@
+import java.util.Map;
+
+/**
+ * Created by felansu on 6/15/15.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        Map<String, String> primeiraInstancia = Configuracao.getConfiguracoes();
+
+        Map<String, String> segundaInstancia = Configuracao.getConfiguracoes();
+        segundaInstancia.put("Chave","Valor da segunda instância");
+
+        if (primeiraInstancia == segundaInstancia){
+            System.out.println("São a mesma instância");
+            System.out.println(primeiraInstancia);
+            System.out.println(segundaInstancia);
+        }
+    }
+
+}
